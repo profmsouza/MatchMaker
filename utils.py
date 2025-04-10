46d3f620cbe0019cbd39fc5062832ec6560871c2
Márcio Souza <62972594+profmsouza@users.noreply.github.com>
2025-04-10 14:44:48 -0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -2,10 +2,18 @@ from nltk.tokenize import word_tokenize
 from nltk.stem import RSLPStemmer
 from nltk.corpus import stopwords
 
-stemmer = RSLPStemmer()
-stop_words = set(stopwords.words('portuguese'))
+stemmer = None
+stop_words = None
+
+def init_nltk():
+    global stemmer, stop_words
+    if stemmer is None:
+        stemmer = RSLPStemmer()
+    if stop_words is None:
+        stop_words = set(stopwords.words('portuguese'))
 
 def preprocessar_texto(texto: str) -> str:
+    init_nltk()
     palavras = word_tokenize(texto.lower())
     radicais = [stemmer.stem(p) for p in palavras if p.isalpha() and p not in stop_words]
     return " ".join(radicais)
